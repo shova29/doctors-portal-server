b956ca938c2ddc567139aacbd6cdc4a49d02cd5b
shova29 <halima2996@gmail.com>
2022-05-17 05:08:57 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const cors = require("cors");
+const json = require("jsonwebtoken");
+require("dotenv").config();
+const app = express();
+const port = process.env.PORT || 5000;
+
+//middleware
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Doctors Portal Running");
+});
+
+app.listen(port, () => {
+  console.log(`Doctors App Listening to Port ${port}`);
+});
