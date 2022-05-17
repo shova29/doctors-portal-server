019487781c76eccbe61fa78ee194f022666d5ff9
shova29 <halima2996@gmail.com>
2022-05-18 00:11:11 +0600

    service get api added

--- index.js
+++ index.js
@@ -2,12 +2,47 @@ const express = require("express");
 const cors = require("cors");
 const json = require("jsonwebtoken");
 require("dotenv").config();
+const { MongoClient, ServerApiVersion } = require("mongodb");
+
 const app = express();
 const port = process.env.PORT || 5000;
 
 //middleware
 app.use(cors());
 app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.jb2sq.mongodb.net/?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+
+async function run() {
+  try {
+    await client.connect();
+    console.log("Connected to Doctors DB");
+    const serviceCollection = client
+      .db("doctors-portal")
+      .collection("services");
+    app.get("/service", async (req, res) => {
+      const query = {};
+      const cursor = serviceCollection.find(query);
+      const services = await cursor.toArray();
+      res.send(services);
+    });
+  } catch {}
+}
+
+run().catch(console.dir());
+
+/*
+client.connect((err) => {
+  const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  client.close();
+}); */
 
 app.get("/", (req, res) => {
   res.send("Doctors Portal Running");
